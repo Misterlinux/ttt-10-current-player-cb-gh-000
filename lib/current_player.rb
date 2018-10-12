f39127107317179745e60d5e0b7ab9b354e98783
Angelo zarate <mrlzarate8@gmail.com>
2018-10-12 13:40:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,10 @@
 board=[" "," "," "," "," "," "," "," "," "]
 
 def turn_count(board)
-    counter=0
+  counter=0
   board.each do |position|
     if position!=" "
       counter+=1
-    else
-      break
     end
   end
 end

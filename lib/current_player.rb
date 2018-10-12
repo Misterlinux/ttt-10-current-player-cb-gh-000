cae62bd50adb44b5e249f08c225c5595a659a4f3
Angelo zarate <mrlzarate8@gmail.com>
2018-10-12 13:38:22 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+board=[" "," "," "," "," "," "," "," "," "]
+
+def turn_count(board)
+  counter=0
+  board.each do |position|
+    if position!=" "
+      counter+=1
+    else
+      break
+  end
+end
